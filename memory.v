e128804a4f33650f2f9e00d5a9e9e83c0182bab5
Matheus de Alencar <sletezes@gmail.com>
2025-06-30 15:16:45 -0300

    tentativa 1

--- memory.v
+++ memory.v
@@ -14,5 +14,29 @@ module Memory #(
     output wire        ack_o       // Confirmação da transação
 );
 
+    reg [31:0] memory [0:MEMORY_SIZE-1];
+    reg [31:0] _data_o;
+    reg _ack_o;
 
+    initial begin
+        if (MEMORY_FILE != "") begin
+            $readmemh(MEMORY_FILE, memory);
+        end
+    end
+
+    always @(posedge clk) begin
+        _ack_o  <= 1'b0;
+        _data_o <= 32'b0;
+
+        if (rd_en_i) begin
+            _data_o <= memory[addr_i[31:2]];
+            _ack_o  <= 1'b1;
+        end else if (wr_en_i) begin
+            memory[addr_i[31:2]] <= data_i;
+            _ack_o <= 1'b1;
+        end
+    end
+
+    assign data_o = _data_o;
+    assign ack_o  = _ack_o;
 endmodule
--- registers.v
+++ registers.v
@@ -11,5 +11,14 @@ module Registers (
     output wire [31:0] RS2_data_o
 );
 
+    reg[31:0] registers [0:31];
 
+    always @(posedge clk) begin
+        if (wr_en_i && RD_ADDR_i != 5'd0) begin
+            registers[RD_ADDR_i] <= data_i;
+        end
+    end
+
+    assign RS1_data_o = (RS1_ADDR_i == 5'd0) ? 32'b0 : registers[RS1_ADDR_i];
+    assign RS2_data_o = (RS2_ADDR_i == 5'd0) ? 32'b0 : registers[RS2_ADDR_i];
 endmodule
